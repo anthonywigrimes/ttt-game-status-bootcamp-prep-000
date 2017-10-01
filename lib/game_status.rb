431140f6fc851e8113e098b07597fcdcae1b9add
Anthony Grimes <agrimes@charter.net>
2017-10-01 14:25:26 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,7 @@
 #board = ["X", "", "", "X", "", "", "X", "", ""]
 #board = ["X", " ", " ", "X", " ", " ", "X", " ", " "]
-board = ["X", "X", "O", "O", "X", "X", "X", "O", "O"]
+#board = ["X", "X", "O", "O", "X", "X", "X", "O", "O"]
+board = ["X", "O", " ", " ", " ", " ", " ", "O", "X"]
 
 # Helper Method
 def position_taken?(board, index)
